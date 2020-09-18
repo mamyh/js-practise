426ce4d139de0b1b9d654f87692bdbb15e81ce75
mamyh <likhmaon@gmail.com>
2020-09-18 20:24:13 +0600

    all done without babel or webpack

--- script.js
+++ script.js
@@ -184,3 +184,35 @@ HtmlSelectElement.prototype.constructor = HtmlSelectElement;
  }
  HtmlImageElement.prototype = new HtmlElement;
  HtmlImageElement.prototype.constructor = HtmlImageElement;
+
+ /*Excercise 3 Stack solution*/
+ const _items = new WeakMap();
+
+ class Stack{
+ 	constructor(){
+       _items.set(this,[]);
+ 	}
+
+ 	push(obj){
+ 		_items.get(this).push(obj);
+ 	}
+
+ 	pop(){
+ 		const items = _items.get(this);
+ 		if(items.length===0)
+              throw new Error('Stack is empty');
+
+        return items.pop();
+ 	}
+ 	peek(){
+ 		const items = _items.get(this);
+ 		if(items.length === 0)
+ 			throw new Error('Stack is empty');
+
+ 		return items[items.length-1];
+ 	}
+ 	get count(){
+ 		return _items.get(this).length;
+ 	}
+ }
+  const stack = new Stack;
